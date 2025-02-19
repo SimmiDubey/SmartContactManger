24a82930653b0ee4996891755f7b6b0dd56334d6
SimmiDubey <dubeysimmi2002@gmail.com>
2025-02-19 20:01:54 +0530

    project

--- src/main/java/com/example/SmartContactManager/smart/controller/HomeController.java
+++ src/main/java/com/example/SmartContactManager/smart/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.example.SmartContactManager.com.smart.dao.UserRepository;
 import com.example.SmartContactManager.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,9 +12,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String home(){
-        return "home";
+    @RequestMapping("/")
+    public String home(Model model){
+        model.addAttribute("title","Home -Smart Contact manager");
+           return "home";
     }
+
+    @RequestMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title","about -Smart Contact manager");
+        return "about";
+    }
+
+
+    @RequestMapping("/signup")
+    public String signup(Model model){
+        model.addAttribute("title","Register -Smart Contact manager");
+        return "signup";
+    }
+
 
 }
