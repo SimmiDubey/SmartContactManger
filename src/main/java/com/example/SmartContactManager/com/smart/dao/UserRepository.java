0d86a337867d2cd875e126f0d7eb0cb7d5d9452d
SimmiDubey <dubeysimmi2002@gmail.com>
2025-02-25 20:10:59 +0530

    project

--- src/main/java/com/example/SmartContactManager/com/smart/dao/UserRepository.java
+++ src/main/java/com/example/SmartContactManager/com/smart/dao/UserRepository.java
@@ -2,6 +2,13 @@ package com.example.SmartContactManager.com.smart.dao;
 
 import com.example.SmartContactManager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-}
+
+    @Query("select u from  user  User u where u.email = : email")
+    public User getUserByUserName(@Param("email") String email);
+
+ }
+
--- src/main/java/com/example/SmartContactManager/config/MyConfig.java
+++ src/main/java/com/example/SmartContactManager/config/MyConfig.java
@@ -1,9 +1,33 @@
 package com.example.SmartContactManager.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class MyConfig
-{
+@EnableWebSecurity
+public class MyConfig{
+
+    @Bean
+    public UserDetailsService getUserDetailsService(){
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(this.getUserDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+
+    }
+
 
 }
